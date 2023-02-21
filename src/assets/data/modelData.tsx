f4f6a0294d6889712a3523f2f3abd4eb415160a3
Rusiru Gunaratne <info.rusiru@gmail.com>
2023-02-21 13:47:56 +0530

    data added

---
+++ src/assets/data/modelData.tsx
@@ -0,0 +1,31 @@
+const farms = [
+  {
+    Id: 1,
+    Name: "Marvellous",
+    Image:
+      "https://www.slt.lk/sites/default/files/landing_page_banners/4GKeyVisual-lp2.jpg",
+    Latitude: 15.4444,
+    Longitude: 16.2222,
+    HasBarge: true,
+  },
+  {
+    Id: 2,
+    Name: "Marvellous",
+    Image:
+      "https://www.slt.lk/sites/default/files/landing_page_banners/4GKeyVisual-lp2.jpg",
+    Latitude: 15.4444,
+    Longitude: 16.2222,
+    HasBarge: true,
+  },
+  {
+    Id: 3,
+    Name: "Marvellous",
+    Image:
+      "https://www.slt.lk/sites/default/files/landing_page_banners/4GKeyVisual-lp2.jpg",
+    Latitude: 15.4444,
+    Longitude: 16.2222,
+    HasBarge: true,
+  },
+];
+
+export { farms };
