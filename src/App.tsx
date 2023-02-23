cc28f4533aa3bc3d1f706abdcc39dbc03d76ec5e
Rusiru Gunaratne <info.rusiru@gmail.com>
2023-02-23 11:07:29 +0530

    theme completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { Router } from "./routes/Router";
+import { darkTheme } from "./themes/Themes";
 
 function App() {
-  return <Router></Router>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router></Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
